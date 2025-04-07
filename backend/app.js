bf085fb23b809539a3873be63bad14f0f1c45a1e
manav-singhal12 <manavsinghal165@gmail.com>
2025-04-07 21:57:58 +0530

    trying

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app=express();
 
 
 app.use(cors({
-    origin: 'https://solbudget.onrender.com',
+    origin: 'https://solbudget.vercel.app',
     // origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ensure all necessary methods are allowed

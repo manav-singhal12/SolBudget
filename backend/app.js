771f5adf4ad29915080b5c16445eef7d763a6385
manav-singhal12 <manavsinghal165@gmail.com>
2025-04-07 22:01:16 +0530

    trying

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,16 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ensure all necessary methods are allowed
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers if needed
 }));
-app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "website-url"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header(
+//       "Access-Control-Allow-Origin",
+//       "website-url"
+//     );
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+//   });
 
 
 app.use(express.json({

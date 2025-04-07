2a69c95275e36d759936b299d451a5659d49b008
manav-singhal12 <159312978+manav-singhal12@users.noreply.github.com>
2025-04-07 21:21:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app=express();
 
 
 app.use(cors({
-    origin: 'https://solbudget.vercel.app',
+    origin: '*',
     // origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ensure all necessary methods are allowed
